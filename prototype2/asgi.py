09c74bfbb5025839d102a4375becf63e89f51b9c
cody <codycon1@gmail.com>
2023-01-25 13:37:27 -0700

    Initial commit

---
+++ prototype2/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for prototype2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prototype2.settings')
+
+application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": application,
+    # Just HTTP for now. (We can add other protocols later.)
+})
